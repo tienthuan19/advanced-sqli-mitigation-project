487a1692e7568aabbef731706964ba36a7fc7ec3
Le Bui Tien Thuan <lebuithuan@gmail.com>
2026-01-15 22:00:30 +0700

    feat(IpBlacklistRepository): findByFingerprintAndUnblockAtAfter

--- src/main/java/com/cybersecurity/application/repository/IpBlacklistRepository.java
+++ src/main/java/com/cybersecurity/application/repository/IpBlacklistRepository.java
@@ -3,6 +3,10 @@ package com.cybersecurity.application.repository;
 import com.cybersecurity.application.models.IpBlacklist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface IpBlacklistRepository extends JpaRepository<IpBlacklist, Long> {
     IpBlacklist findByIpAddress(String ip);
+    List<IpBlacklist> findByFingerprintAndUnblockAtAfter(String fingerprint, LocalDateTime now);
 }
